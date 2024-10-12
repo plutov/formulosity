fc83ae889272d7042f5cb2bb1bbda30397be528c
krishna <krishnateja262@gmail.com>
2024-10-12 17:13:56 +0530

    adding schema changes for postgres

--- api/migrations/postgres/000001_schema.up.sql
+++ api/migrations/postgres/000001_schema.up.sql
@@ -57,3 +57,13 @@ CREATE TABLE
     );
 
 CREATE UNIQUE INDEX surveys_answers_unique ON surveys_answers (session_id, question_id);
+
+CREATE TABLE
+    surveys_webhook_responses (
+        id serial NOT NULL PRIMARY KEY,
+        created_at timestamp without time zone default (now () at time zone 'utc'),
+        session_id integer NOT NULL,
+        response_status integer NOT NULL,
+        response TEXT,
+        CONSTRAINT fk_surveys_webhooks1 FOREIGN KEY (session_id) REFERENCES surveys_sessions (id) ON DELETE CASCADE,
+    );
